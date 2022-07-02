284e10494f2cd16cec58d8029c52684cdf4e6c36
BiteTheDDDDt <pxl290@qq.com>
2022-07-02 23:13:40 +0800

    modify log, assign each container lowest resource

--- optimize/optimizer.py
+++ optimize/optimizer.py
@@ -1,11 +1,5 @@
-# edit this file
-# 编辑此文件!
 import json
 
-# 一个可以应用于Istio服务网格平台的Sidecar资源示例
-# 有关Sidecar和EnvoyFilter资源，可参考：
-# https://istio.io/latest/zh/docs/reference/config/networking/sidecar/
-# https://istio.io/latest/zh/docs/reference/config/networking/envoy-filter/
 sidecar_example = '''
 apiVersion: networking.istio.io/v1alpha3
 kind: Sidecar
@@ -18,52 +12,55 @@ spec:
     - "istio-system/*"
 '''
 
-# 修改此方法来帮助评分程序了解何时应该开始测试，当自己的系统准备好接受输入时返回200，否则返回500
-# 准备时间不能超过10分钟，否则将超时直接记为0分
+
 def ready():
     return 200
 
-# 修改此方法实现自己的网格资源优化逻辑
+
 def optimize(containers, accesslog_path, cpu_limit, memory_limit):
+    print("cpu_limit: ", cpu_limit)
+    print("memory_limit: ", memory_limit)
+
+    print("containers size: ", len(containers))
+    for container in containers:
+        print("container: ", container)
+        # 'service_name' Pod所属的服务名称
+        # 'ip'           Pod的IP
+        # 'pod_name'     Pod的名称
+        # 'container'    Pod内容器的名称，名称为istio-proxy的容器就是Sidecar容器
+        # 'cpu'          容器在上一轮测试中的平均cpu占用，单位：核
+        # 'memory'       容器在上一轮测试中的平均内存占用，单位：Byte
+
     accesslogs = []
-    print(cpu_limit) # 当前可分配给Sidecar的CPU核数上限
-    print(memory_limit) # 当前可分配给Sidecar的内存上限
-    # 读取访问日志文件
     with open(accesslog_path, 'r', encoding='utf-8') as accesslog_file:
         for line in accesslog_file:
             accesslog = json.loads(line)
-            print(accesslog['duration']) # 请求时延，e.g. 28
-            print(accesslog['downstream_remote_address']) # 发送方Pod IP、端口，e.g. 10.41.0.79:44750
-            print(accesslog['upstream_host']) # 接收方Pod IP、端口，e.g. 10.41.0.139:9080
-            print(accesslog['bytes_sent']) # 请求大小，e.g. 39
-            print(accesslog['bytes_received']) # 响应大小，e.g. 0
-            print(accesslog['path']) # 请求路径，e.g. /reviews
             accesslogs.append(accesslog)
-    # 读取集群中的容器信息及资源消耗情况
+            print("accesslog: ", accesslog)
+            # 'duration'                  请求时延，e.g. 28
+            # 'downstream_remote_address' 发送方Pod IP、端口，e.g. 10.41.0.79:44750
+            # 'upstream_host'             接收方Pod IP、端口，e.g. 10.41.0.139:9080
+            # 'bytes_sent'                请求大小，e.g. 39
+            # 'bytes_received'            响应大小，e.g. 0
+            # 'path'                      请求路径，e.g. /reviews
+
+    resources = []
     for container in containers:
-        print(container['service_name']) # Pod所属的服务名称
-        print(container['ip']) # Pod的IP
-        print(container['pod_name']) # Pod的名称
-        print(container['container']) # Pod内容器的名称，名称为istio-proxy的容器就是Sidecar容器
-        print(container['cpu']) # 容器在上一轮测试中的平均cpu占用，单位：核
-        print(container['memory']) # 容器在上一轮测试中的平均内存占用，单位：Byte
-    
+        resource = {
+            'service': container['service_name'], 'cpu': 0.1, 'memory': 128000000}
+        resources.append(resource)
+
     optimize_result = {
-        'resource': [ # 通过resource字段为Sidecar容器分配资源，分配的总资源量分别不得超过CPU和内存资源上限
-            {
-                'service': containers[0]['service_name'], # 需要修改资源分配的Sidecar所属服务
-                'cpu': 0.1, # 需要为该服务Sidecar设置的CPU资源上限，单位：核
-                'memory': 512000000# 需要为该服务Sidecar设置的内存资源上限，单位：Byte
-            }
-        ],
-        'istio_cr': [sidecar_example], # 通过istio_cr字段向服务网格应用Sidecar或EnvoyFilter资源来进行优化
+        'resource': resources,
+        # 通过istio_cr字段向服务网格应用Sidecar或EnvoyFilter资源来进行优化
+        'istio_cr': [sidecar_example],
         'features': {
             # 设置使用intel multi-buffer技术加快网格tls通信
             # 参考：https://help.aliyun.com/document_detail/349282.html
-            'multi_buffer': { 
+            'multi_buffer': {
                 'enabled': True,
                 'poll_delay': '0.2s'
             }
         }
-    } 
+    }
     return optimize_result
