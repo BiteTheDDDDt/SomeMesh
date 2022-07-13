e0ed30048f5781fd1a9f7d04eecb70499ea7c2fa
BiteTheDDDDt <pxl290@qq.com>
2022-07-14 00:09:36 +0800

    update

--- optimize/optimizer.py
+++ optimize/optimizer.py
@@ -141,8 +141,8 @@ def optimize(containers, accesslog_path, cpu_limit, memory_limit):
         if request_number_sum > 0 and request_number_map[service_name] > 0:
             resource_map[service_name]['cpu'] += 1.0*request_number_map[service_name] / \
                 request_number_sum*cpu_limit/size-eps
-            resource_map[service_name]['memory'] += int(
-                request_number_map[service_name]/request_number_sum*memory_limit/size)
+            # resource_map[service_name]['memory'] += int(
+            #    request_number_map[service_name]/request_number_sum*memory_limit/size)
 
     resources = [resource_map[service_name] for service_name in resource_map]
 
