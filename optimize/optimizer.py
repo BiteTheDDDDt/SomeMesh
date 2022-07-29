35e66b78ddd5cb8fbd3d762324e9c331a75f80c1
BiteTheDDDDt <pxl290@qq.com>
2022-07-30 01:04:51 +0800

    update

--- optimize/optimizer.py
+++ optimize/optimizer.py
@@ -11,7 +11,7 @@ spec:
   template:
     metadata:
       annotations:
-        proxy.istio.io/config: 'concurrency: 3'
+        proxy.istio.io/config: 'concurrency: 1'
   egress:
   - hosts:
     - "./*"
