fb52ac0d2ce48f78bbd49ba21d96a76a54ce126b
BiteTheDDDDt <pxl290@qq.com>
2022-08-10 20:26:35 +0800

    update

--- optimize/optimizer.py
+++ optimize/optimizer.py
@@ -85,7 +85,7 @@ def update_request(service_map, ip, ip_to_service, request_number_map, request_b
     if ip in ip_to_service:
         service_name = ip_to_service[ip]
         request_number_map[service_name] += 1000.0 / \
-            (len(service_map[service_name])**(0.8))
+            (len(service_map[service_name])**(0.3))
         request_byte_map[service_name] += accesslog['bytes_sent'] + \
             accesslog['bytes_received']
 
@@ -152,7 +152,7 @@ def optimize(containers, accesslog_path, cpu_limit, memory_limit):
         'features': {
             'multi_buffer': {
                 'enabled': True,
-                'poll_delay': '3s'
+                'poll_delay': '0.5s'
             }
         }
     }
