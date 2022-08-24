6838d49ce611ed559c14f5c6f3d0bce95b474ee5
BiteTheDDDDt <pxl290@qq.com>
2022-08-24 23:29:29 +0800

    update

--- optimize/optimizer.py
+++ optimize/optimizer.py
@@ -85,7 +85,7 @@ def update_request(service_map, ip, ip_to_service, request_number_map, request_b
     if ip in ip_to_service:
         service_name = ip_to_service[ip]
         request_number_map[service_name] += 1000.0 / \
-            (len(service_map[service_name])**(0.5))
+            (len(service_map[service_name])**(0.4))
         request_byte_map[service_name] += accesslog['bytes_sent'] + \
             accesslog['bytes_received']
 
@@ -133,7 +133,7 @@ def optimize(containers, accesslog_path, cpu_limit, memory_limit):
     request_byte_sum = sum([request_byte_map[service_name]
                            for service_name in request_byte_map])
 
-    cpu_limit *= 0.7
+    cpu_limit *= 0.75
     memory_limit *= 0
 
     for service_name in service_map:
@@ -142,7 +142,7 @@ def optimize(containers, accesslog_path, cpu_limit, memory_limit):
             resource_map[service_name]['cpu'] += 1.0*request_number_map[service_name] / \
                 request_number_sum*cpu_limit/size-eps
             resource_map[service_name]['memory'] += int(
-               request_number_map[service_name]/request_number_sum*memory_limit/size)
+                request_number_map[service_name]/request_number_sum*memory_limit/size)
 
     resources = [resource_map[service_name] for service_name in resource_map]
 
